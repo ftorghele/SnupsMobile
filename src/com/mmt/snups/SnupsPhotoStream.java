83be82e0265cc3f29dfbcfe7bf0870dd00b00eb0
Franz Torghele <f.torghele@gmail.com>
2011-04-13 22:24:48 +0200

    fixed webview link problem

--- src/com/mmt/snups/SnupsPhotoStream.java
+++ src/com/mmt/snups/SnupsPhotoStream.java
@@ -1,71 +1,55 @@
 package com.mmt.snups;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.webkit.WebSettings;
+import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
  
-public class SnupsPhotoStream extends Activity {
-    private WebView webview;
-    private static final String TAG = "PhotoStream";
-    private ProgressDialog progressBar;  
+public class SnupsPhotoStream extends Activity
+{
+    final Activity activity = this;
  
-    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
- 
- 
+        this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
         setContentView(R.layout.snups_photo_stream);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
  
-        this.webview = (WebView)findViewById(R.id.webView);
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress)
+            {
+                activity.setProgress(progress * 100);
  
-        WebSettings settings = webview.getSettings();
-        settings.setJavaScriptEnabled(false);
-        webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
- 
-        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
- 
-        progressBar = ProgressDialog.show(SnupsPhotoStream.this, "Snups", "Loading...");
- 
-        webview.setWebViewClient(new WebViewClient() {
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                Log.v(TAG, "Processing webview url click...");
-                view.loadUrl(url);
-                return true;
-            }
- 
-            public void onPageFinished(WebView view, String url) {
-                Log.v(TAG, "Finished loading URL: " +url);
-                if (progressBar.isShowing()) {
-                    progressBar.dismiss();
-                }
-            }
- 
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                Log.v(TAG, "Error: " + description);
-                alertDialog.setTitle("Error");
-                alertDialog.setMessage(description);
-                alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        return;
-                    }
-                });
-                alertDialog.show();
+                if(progress == 100)
+                    activity.setTitle(R.string.app_name);
             }
         });
-        webview.loadUrl("http://franzonrails.multimediatechnology.at/mobilehome");
+ 
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl)
+            {
+                // Handle the error
+            }
+ 
+
+
+        });
+ 
+        
+        webView.loadUrl("file:///android_asset/www/photostream.html");
     }
+
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
