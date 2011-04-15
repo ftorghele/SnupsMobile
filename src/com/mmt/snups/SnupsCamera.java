fa1849ecf46a274cc6c9e241697dccd080164903
Joe-Z <franz.lanzendorfer@gmx.at>
2011-04-15 15:45:13 +0200

    photo is taken with android camera-app and stored via Uri

--- src/com/mmt/snups/SnupsCamera.java
+++ src/com/mmt/snups/SnupsCamera.java
@@ -1,27 +1,48 @@
 package com.mmt.snups;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.widget.Toast;
 
 public class SnupsCamera extends Activity {
+	Uri outputFileUri;
 
 	public void onCreate(Bundle icicle) {
 		Log.e("SnupsCamera", "onCreate");
 		
 		super.onCreate(icicle);
 		
+		//kick off the camera application
 		try {
-			Intent action = new Intent("android.media.action.IMAGE_CAPTURE");
-			startActivityForResult(action,1);
+			Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            
+            File imageFile = new File(Environment.getExternalStorageDirectory(), "snups.jpg");
+            outputFileUri = Uri.fromFile(imageFile);
+            
+//            outputFileUri = getContentResolver().insert(Media.EXTERNAL_CONTENT_URI, values);
+			
+            Log.v("external filepath:", Environment.getExternalStorageDirectory().toString());
+			Log.v("uri-pfad:", outputFileUri.getPath());
+            
+			//we have to tell the camera-app where to store the image
+			//otherwise we just get a preview-image in onActivityResult()
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            
+			startActivityForResult(intent,1);
 		} catch (Exception e) {
 			Log.e(SnupsMission.class.getName(),"Error occured [" + e.getMessage() + "]");
 		}
@@ -30,28 +51,41 @@ public class SnupsCamera extends Activity {
 	
     protected void onActivityResult(int requestCode,int resultCode,Intent data)
     {
-    	Bitmap b = null;
     	try {
-    		if (requestCode == 1) {
-    			Log.i(SnupsMission.class.getName(),"resultCode is [" + resultCode + "]");
-    			if (resultCode == RESULT_OK) {
-	    			if (b != null) b.recycle();
-	    			b = (Bitmap) data.getExtras().get("data");
-	    			if (b != null) {
-	    				StoreByteImage(this, b, 80);
-	    				
-	    				Intent intent=new Intent(getApplicationContext(),SnupsUpload.class);
-	    		        startActivity(intent);
-	    		        finish();
-	    			}
-    			}
-    			else {
-    				Intent intent=new Intent(getApplicationContext(),SnupsMission.class);
-    		        startActivity(intent);
-    		        finish();
-    			}
+    		if (requestCode == 1 && resultCode == RESULT_OK) {
+    			Uri selectedImage = outputFileUri;
+                getContentResolver().notifyChange(selectedImage, null);
+                ContentResolver cr = getContentResolver();
+                
+                Bitmap bitmap;
+                try {
+                     bitmap = android.provider.MediaStore.Images.Media
+                     .getBitmap(cr, selectedImage);
+                     
+                     StoreByteImage(this, bitmap, 80);
+                     
+                     Intent intent=new Intent(getApplicationContext(),SnupsUpload.class);
+                     startActivity(intent);
+                     finish();
+                     
+//                     Log.v("bitmap:", bitmap.toString());
+
+//                    Toast.makeText(this, selectedImage.toString(),
+//                            Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+                            .show();
+                    Log.e("Camera", e.toString());
+                }
     		}
-    	}catch (Exception e) {
+			else {
+				Log.i(SnupsMission.class.getName(),"resultCode is [" + resultCode + "]");
+				Intent intent=new Intent(getApplicationContext(),SnupsMission.class);
+		        startActivity(intent);
+		        finish();
+			}
+    	}
+    	catch (Exception e) {
     		Log.e(SnupsMission.class.getName(),"onActivityResult Error [" + e.getMessage() + "]");
     	}
     	
@@ -60,7 +94,7 @@ public class SnupsCamera extends Activity {
 	public static boolean StoreByteImage(Context mContext, Bitmap myImage,
 			int quality) {
 		
-		String filePath = "image.jpeg";
+		String filePath = "snupsImage.jpeg";
 		
 		Log.v("filePath:", filePath);
 
@@ -71,10 +105,11 @@ public class SnupsCamera extends Activity {
 			
 			myImage.compress(CompressFormat.JPEG, quality, fos);
 			
-			fos.flush();
+//			needed?:			
+//			fos.flush();
 
 //			needed?:
-//			fos.close();
+			fos.close();
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
